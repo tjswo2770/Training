a1e84f22541286dca8ac639533ac8860cd290dcb
선재 이 <dltjswo273@naver.com>
2021-01-05 11:04:46 +0900

    첫번째 솔루션을 만들어보았다..

---
+++ TR1_Enum/TR1_Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR1_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////int a = 1;
+            //CrossAppDomainDelegatedd
+            //    d
+            //    d
+            //    d
+            //    d
+
+        }
+    }
+}
