da5d065922beea089c3a465646ea9f9be5455195
선재 이 <dltjswo273@naver.com>
2021-01-05 15:28:09 +0900

    .

--- TR_2Class/TR_2Class/Properties/AssemblyInfo.cs
+++ TR_2Class/TR_2Class/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TR_2Class")]
+[assembly: AssemblyProduct("TR2_Class")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
