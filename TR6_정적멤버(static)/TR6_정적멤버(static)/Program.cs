77bd923ccd009fa265e28abf9e33a237b9e28339
선재 이 <dltjswo273@naver.com>
2021-01-06 13:21:19 +0900

    static

---
+++ TR6_정적멤버(static)/TR6_정적멤버(static)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR6_정적멤버_static_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
